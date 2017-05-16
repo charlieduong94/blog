d8b0b7147089b5955221b20155e37f08cab6213a
Charlie Duong <charlieduong94@gmail.com>
2017-05-15 20:31:21 -0400

    Build script now generates a properly linked static site

--- project.js
+++ project.js
@@ -6,11 +6,10 @@ require('marko/browser-refresh').enable()
 require('lasso/browser-refresh').enable('*.marko *.css *.less')
 
 const Koa = require('koa')
-const Router = require('koa-path-router')
 const serve = require('koa-static')
 const mount = require('koa-mount')
 
-const mkdirp = require('mkdirp')
+const nodeMkdirp = require('mkdirp')
 const fs = require('fs')
 const logger = require('~/src/logging').logger(module)
 const yaml = require('js-yaml')
@@ -21,8 +20,9 @@ const META_DATA_DELIMITER = '---'
 const POSTS_INPUT_DIR = `${__dirname}/posts`
 const OUTPUT_DIR = `${__dirname}/dist`
 const POSTS_OUTPUT_DIR = `${OUTPUT_DIR}/post`
+const PAGES_OUTPUT_DIR = `${OUTPUT_DIR}/page`
 const STATIC_DIR = `${OUTPUT_DIR}/static`
-const isProduction = true
+const MAX_POSTS_PER_PAGE = 5
 
 require('lasso').configure({
   plugins: [
@@ -30,20 +30,31 @@ require('lasso').configure({
     'lasso-less'
   ],
   outputDir: STATIC_DIR,
-  bundlingEnabled: isProduction,
-  minify: isProduction,
-  fingerprintsEnabled: isProduction
+  bundlingEnabled: true,
+  minify: true,
+  fingerprintsEnabled: true
 })
 
 const PostPage = require('~/src/pages/post')
-const PostListPage = require('~/src/pages/index')
+const PostListPage = require('~/src/pages/post-list')
 const renderMarkdown = require('~/src/util/renderMarkdown')
 
 function calcPostReadTime (rawPostContent) {
   return Math.ceil(rawPostContent.match(wordRegex).length / AVG_WORDS_READ_PER_MIN)
 }
 
-async function readFileAsync (fileName) {
+async function mkdirp (directory) {
+  return new Promise((resolve, reject) => {
+    nodeMkdirp(directory, (err) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve()
+    })
+  })
+}
+
+async function readFile (fileName) {
   return new Promise((resolve, reject) => {
     return fs.readFile(fileName, 'utf8', (err, data) => {
       if (err) {
@@ -54,8 +65,20 @@ async function readFileAsync (fileName) {
   })
 }
 
+async function writeFile (stream, outputDir) {
+  const writeStream = fs.createWriteStream(outputDir)
+  const writePromise = new Promise((resolve, reject) => {
+    writeStream.on('finish', resolve)
+    writeStream.on('error', reject)
+  })
+
+  stream.pipe(writeStream)
+
+  return writePromise
+}
+
 async function parsePost (filePath) {
-  const rawPost = await readFileAsync(filePath)
+  const rawPost = await readFile(filePath)
   const rawPostMetaData = rawPost.slice(0, rawPost.indexOf(META_DATA_DELIMITER))
   const rawPostContent = rawPost.slice(rawPost.indexOf(META_DATA_DELIMITER) +
     META_DATA_DELIMITER.length)
@@ -69,122 +92,100 @@ async function parsePost (filePath) {
   return postData
 }
 
-async function writePost (postData, outputFileName) {
+async function writePost (postData, outputDir) {
+  logger.info('Generating post', outputDir)
   const postStream = PostPage.stream(postData)
-  const writeStream = fs.createWriteStream(`${POSTS_OUTPUT_DIR}/${outputFileName}`)
+  return writeFile(postStream, `${outputDir}/index.html`)
+}
 
-  const writePromise = new Promise((resolve, reject) => {
-    writeStream.on('finish', resolve)
-    writeStream.on('error', reject)
+async function writePage (options, outputDir) {
+  const {
+    posts,
+    nextPage,
+    prevPage
+  } = options
+
+  logger.info('Generating page', outputDir)
+  await mkdirp(outputDir)
+
+  const pageStream = PostListPage.stream({
+    posts,
+    nextPageLink: nextPage && `/page/${nextPage}`,
+    prevPageLink: prevPage && `/page/${prevPage}`
   })
 
-  postStream.pipe(writeStream)
+  return writeFile(pageStream, `${outputDir}/index.html`)
+}
 
-  return writePromise
+async function buildPages (posts) {
+  let lastIndex = 0
+  let currentPage = 1
+  let pageOutputDir = `${PAGES_OUTPUT_DIR}/${currentPage}`
+
+  for (let i = 1; i <= posts.length; i++) {
+    if (i % MAX_POSTS_PER_PAGE === 0) {
+      const prevPage = currentPage - 1
+      const nextPage = lastIndex + MAX_POSTS_PER_PAGE < posts.length ? currentPage + 1 : undefined
+
+      await writePage({
+        posts: posts.slice(lastIndex, i),
+        prevPage,
+        nextPage
+      }, pageOutputDir)
+
+      lastIndex = i
+      currentPage++
+      pageOutputDir = `${PAGES_OUTPUT_DIR}/${currentPage}`
+    }
+  }
+
+  if (lastIndex < posts.length) {
+    await writePage({
+      posts: posts.slice(lastIndex),
+      prevPage: currentPage - 1
+    }, pageOutputDir)
+  }
+
+  // for now, just copy the first page over to the index page
+  logger.info('Generating index file', `${OUTPUT_DIR}/index.html`)
+  await writeFile(fs.createReadStream(`${PAGES_OUTPUT_DIR}/1/index.html`), `${OUTPUT_DIR}/index.html`)
 }
 
 exports.build = async () => {
-  mkdirp.sync(POSTS_OUTPUT_DIR)
   const posts = fs.readdirSync(POSTS_INPUT_DIR)
   const parsedPosts = []
   const pages = []
   const pageIndex = 0
+  const work = []
 
-  // build each of the individual posts
+  // build and track each of the individual posts
   for (const post of posts) {
     try {
       const endPos = post.lastIndexOf('.')
-      const outputFileName = `${post.slice(0, endPos)}.html`
 
-      logger.info('Generating post', outputFileName)
-
+      const postName = post.slice(0, endPos)
       const postData = await parsePost(`${POSTS_INPUT_DIR}/${post}`)
-      parsedPosts.push({
-        title: postData.title,
-        date: postData.date,
-        description: postData.description
-      })
+      postData.path = `/post/${postName}`
 
-      await writePost(postData, outputFileName)
+      parsedPosts.push(postData)
+      const baseDir = `${POSTS_OUTPUT_DIR}/${postName}`
+      await writePost(postData, baseDir)
     } catch (err) {
       throw err
     }
   }
+
+  await Promise.all(work)
+
+  // build pages based off of the posts
+  return buildPages(parsedPosts)
 }
 
 exports.serve = async (port) => {
+  await this.build()
+
   const app = new Koa()
-  const router = new Router()
-
-  const posts = fs.readdirSync(POSTS_INPUT_DIR)
-  const parsedPosts = []
-
-  for (const post of posts) {
-    let data = await parsePost(`${POSTS_INPUT_DIR}/${post}`)
-    data.link = `/post/${post.slice(0, post.lastIndexOf('.'))}`
-    parsedPosts.push(data)
-  }
-
-  router.register({
-    path: '/',
-    async handler (ctx) {
-      const posts = fs.readdirSync(POSTS_INPUT_DIR)
-
-      ctx.set('Content-Type', 'text/html')
-      ctx.body = PostListPage.stream({
-        posts: parsedPosts.slice(0, 5),
-        nextPage: parsedPosts.length > 5 ? 2 : undefined
-      })
-    }
-  })
-
-  router.register({
-    path: '/posts/:pageNum',
-    async handler (ctx) {
-      const [ pageNum ] = ctx.params
-      const page = +pageNum
-
-      if (page === 1) {
-        ctx.redirect('/')
-      } else {
-        let postIndex = page - 1
-        let startPos = 5 * postIndex
-        let endPos = (5 * postIndex) + 5
-        let postsSlice = parsedPosts.slice(startPos, endPos)
-
-        let prevPage = page - 1
-        let nextPage = endPos < parsedPosts.length ? page + 1 : undefined
-
-        ctx.set('Content-Type', 'text/html')
-        ctx.body = PostListPage.stream({
-          posts: postsSlice,
-          prevPage,
-          nextPage
-        })
-      }
-    }
-  })
-
-  router.register({
-    path: '/post/:postName',
-    async handler (ctx) {
-      const [ postName ] = ctx.params
-      const fileName = `${postName}.md`
-      logger.info('Generating', fileName)
-
-      try {
-        ctx.set('Content-Type', 'text/html')
-        const postData = await parsePost(`${POSTS_INPUT_DIR}/${fileName}`)
-        ctx.body = PostPage.stream(postData)
-      } catch (err) {
-        logger.error(err)
-        ctx.body = err.message
-      }
-    }
-  })
-
-  app.use(router.getRequestHandler())
-  app.use(serve(OUTPUT_DIR))
+  app.use(mount('/', serve(OUTPUT_DIR)))
 
   return new Promise((resolve) => {
     app.listen(port, () => {
